889c497b2bc1ad63d962af5288af21000d7db02b
vithuydiep <diepvi2810@gmail.com>
2023-05-03 09:58:47 +0700

    change gitignore

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Vidt2000
+export POSTGRES_HOST=vidt6.cmupgoimn4kf.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=vidt6
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
